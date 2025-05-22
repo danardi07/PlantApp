2063231ccc91d7937919bd34d128a035085de05f
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-05-22 22:49:29 +0700

    membuat class plant yang berisi judul, negara, foto dan harga

---
+++ lib/services/plant.dart
@@ -0,0 +1,8 @@
+class Plant {
+  final String title;
+  final String country;
+  final String image;
+  final int price;
+
+  Plant({required this.title, required this.country, required this.price, required this.image});
+}
